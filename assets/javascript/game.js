dd4dc5390534ef9f494aac0c1835617623ab30bc
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-05-18 00:59:08 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,176 @@
-var baseballTeam = ["chicago cubs", "los angeles dodgers", "new york yankess", "baltimore orioles",
-	"cleveland indians", "toronto blue jays", "boston red sox", "new york mets", 
-	"san francisco giants", "st louis cardinals", "detroit tigers", "texas rangers",
-	"atlanta braves", "pittsburgh pirates", "houston astros", "angels of anaheim",
-	"philadelphia phillies", "kansas city royals", "washinton nationals", "minnesota twins",
-	"seattle mariners", "cincinnati reds", "miami marlins", "chicago white sox", "colorado rokies",
-	"milwaukee brewers", "arizona diamondbacks", "san diego padres", "oakland athletics",
-	"tampa bay rays"];
+var game = {
+	started: false,
+	currentWord: "",
+	wins: 0, 
+	losses: 0,
+	lives: 8,
+	previousGuesses: [],
+	lettersToGuess: 0,
+	words: ["background", "background-attachment", "background-color", "background-image",
+	"background-position", "background-repeat", "background-size", "border-color", 
+	"border-radius", "border-style", "border-width", "box-shadow", "margin-bottom",
+	"max-height", "min-width", "padding-bottom", "padding-top", "padding-left", "padding-right",
+	"vertical-align", "letter-spacing", "align-content", "justify-content", "overflow-wrap",
+	"line-height", "text-align", "text-transform", "text-decoration", "text-shadow",
+	"font-family", "font-size", "text-orientation", "list-style-image", "animation",
+	"animation-duration", "transform-style", "image-orientation", "border-bottom-style",
+	"margin-right", "padding-left", "z-index", "width", "visibility", "transition",
+	"transition-delay", "transition-duration", "transform", "perspective", "overflow-x",
+	"overflow-y", "outline-style", "margin-top", "marging-left", "list-style-position",
+	"float", "font", "height", "display", "direction", "clear", "bottom", "clip", "border"],
 
-var started = false;
-var currentWord = "", wins = 0, remaining = 10;
-var lettersToGuess = 0;
-var lettersGuessed = [];
 
-var ul = document.getElementById("pattern");
-var list = ul.getElementsByTagName("li");
+	display: function(){
+		
+		//select a randon word from words array and store the value on currentWord variable
+		 this.currentWord = this.words[Math.floor(Math.random() * this.words.length)];
+		
+		var ul = document.getElementById("pattern");
+		ul.innerHTML = "";
 
-
-function displayPattern(team){
-	currentWord = team[Math.floor(Math.random() * team.length)];
-	
-	ul.innerHTML = "";
-
-	for(var i = 0; i < currentWord.length; i++){
-		var li = document.createElement("li");
-		if(currentWord[i] != " "){
-			li.innerHTML = "_";
-			lettersToGuess++;
-		} else{
-			li.innerHTML = "-";
+		for(var i = 0; i < this.currentWord.length; i++){
+			var li = document.createElement("li");
+			if(this.currentWord[i] != "-"){
+				li.innerHTML = "_";
+				this.lettersToGuess++;
+			} else{
+				li.innerHTML = "-";
+			}
+			ul.appendChild(li);
 		}
-		ul.appendChild(li);
-	}
-}
+		this.createBtn();
+	},
 
-function letterSearch(letter){
-	
-	var found = false;
-	for(var i = 0; i < currentWord.length; i++){
-		if(currentWord[i] === letter){
-			list[i].textContent = letter;
-			lettersToGuess --;
-			found = true;
+
+	//This function check if the letter typed is inside the current word and return true or false
+	search: function (char){
+		if(this.previousGuesses.indexOf(char) == -1){
+			this.previousGuesses.push(char);
+			
+			var ul = document.getElementById("pattern");
+			var list = ul.getElementsByTagName("li");
+			var found = false;
+			for(var i = 0; i < this.currentWord.length; i++){
+				if(this.currentWord[i] === char){
+					list[i].textContent = char;
+					this.lettersToGuess --;
+					found = true;
+				}
+			}
+
+			var btn = document.getElementById(char);
+			btn.className += " disabled";
+
+			if(found){
+				this.rightAnswer();
+				btn.className += " right";
+			}else{
+				this.wrongAnswer();
+				btn.className += " wrong";
+			}
+			return found;
 		}
-	}
-	return found;
-}
+	},
 
-function newGame(){
-	remaining = 10;
-	lettersToGuess = 0;
-	lettersGuessed = [];
-	document.getElementById("remaining").innerHTML = remaining;
-	document.getElementById("guessed").innerHTML = "";
-	displayPattern(baseballTeam);
+	wrongAnswer: function(){
+		this.lives --;
+		document.getElementById("lives").innerHTML = this.lives;
+		if(this.lives == 0){
+ 		 	this.msg('You LOST. The word was "' +  this.currentWord +'"', "#ee5f5b");
+			this.losses ++;
+			var losses = document.getElementById("losses");
+			losses.innerHTML = this.losses;
+			this.newGame();
+		}
+	},
 
-}
+	rightAnswer: function(){
+		console.log(this.lettersToGuess);
+		if(this.lettersToGuess == 0){
+ 		 	this.msg("Good work, You WON!", "#62c462");
+			this.wins ++;
+			var wins = document.getElementById("wins");
+			wins.innerHTML = this.wins;
+			this.newGame();
+		}
+	},
 
-function setWins(){
-	var wins = document.getElementById("wins");
-	wins.innerHTML = parseInt(wins.innerHTML) + 1;
-}
+	newGame: function (){
+		this.lives = 8;
+		this.lettersToGuess = 0;
+		this.previousGuesses = [];
+		this.createBtn();
+		document.getElementById("lives").innerHTML = this.lives;
+		this.display();
+	},
+
+	msg: function(msg, bgColor){
+		document.getElementById("msg").innerHTML = msg;
+		var msgC = document.getElementById("msgContainer");
+		msgC.style.backgroundColor = bgColor;
+		msgC.style.display = "block";
+	 	msgC.style.height = "64px";
+
+	 	window.setTimeout(function(){
+	 		msgC.style.height = 0;
+	 	}, 3000);
+	},
+
+	start: function(){
+		document.getElementById("intro").style.display = "none";
+		document.getElementById("game").style.display = "block";
+		document.getElementById("keys").style.display = "block";
+		window.setTimeout(function(){
+			document.getElementById("play").style.display = "none"}, 3000);
+
+		this.started = true;
+		this.display();
+	},
+
+	createBtn: function() {
+		var el = "";
+
+		var letters = {row1: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+					   row2: ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+				       row3: ["z", "x", "c", "v", "b", "n", "m"]};
+
+		for(var key in letters){
+			el += '<div class="btn-group">';
+			for(var i = 0; i < letters[key].length; i++){
+				 el += '<span id="' + letters[key][i] + '" class="btn btn-default">' + 
+				 letters[key][i] + '</span>'
+			}
+			el += '</div>'
+		}
+		document.getElementById("keys").innerHTML = el;
+	},
+
+
+}; //end of object
+
 
 document.onkeyup = function(event) {
- 	if(!started){
- 		started = true;
- 		displayPattern(baseballTeam);
- 	} else{
- 		if( lettersGuessed.indexOf(event.key) == -1){
- 			lettersGuessed.push(event.key);
- 			document.getElementById("guessed").innerHTML = lettersGuessed.join(", ");
- 			
- 			if(!letterSearch(event.key)){
- 				remaining --;
- 				document.getElementById("remaining").innerHTML = remaining;
- 				if(remaining == 0){
- 					alert("your dead");
- 				}		
- 			} else{
- 				if(lettersToGuess == 0){
- 					setWins();
- 					newGame();
- 				}
- 			}
- 		} 
- 	}
- }
+	if(!game.started){
+		game.start();
+	} else{
+		if(event.keyCode > 64 && event.keyCode < 91){
+			var input = String.fromCharCode(event.keyCode).toLowerCase();
+			game.search(input);
+		}
+	}
+};
+
+document.getElementById("play").onclick = function(){
+	game.start();
+};
+
+var btns = document.getElementById("keys");
+btns.addEventListener("click",function(e){
+	if (e.target !== e.currentTarget){
+		 var input = e.target.id;
+		 this.onclick = null;
+		
+		game.search(input);
+	}
+	e.stopPropagation();
+},false);
